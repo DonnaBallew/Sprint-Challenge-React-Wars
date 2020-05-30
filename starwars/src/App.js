391532a7093b9a4e23235427b4191e89a0d71c38
Donna Ballew <ballewdd@gmail.com>
2020-05-29 21:03:11 -0700

    Stretch attempted:  Added 2 seperate components.  They showed but ran out of time

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Character } from "../src/components/Character";
+import { Search } from "../src/components/Search";
+import { SearchTerm } from "../src/components/SearchTerm";
 import "./App.css";
 import styled from "styled-components";
 
@@ -44,6 +46,8 @@ const App = () => {
           <Character props={characters} key={i} />
         ))}
       </Container>
+      <Search />
+      <SearchTerm />
     </div>
   );
 };
